c253fad7a37e28fd51c11555d96cd31c858a4aca
jayuth <teerapong.uthairat.1877@gmail.com>
2019-09-24 16:23:53 -0400

    reacting to changes module

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
+import { debounceTime } from 'rxjs/operators';
+
 import { Customer } from './customer';
 
 // custom validator with paramerter 
@@ -20,7 +22,8 @@ function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
   // extract a value from each FormControl name
   const emailControl = c.get('email');
   const confirmControl = c.get('confirmEmail');
-  
+
+  // wait until the user finished filling both email and confirmEmail before performing validation
   if(emailControl.pristine || confirmControl.pristine){
     return null;
   }
@@ -36,10 +39,19 @@ function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
+
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   // data model
   customer = new Customer();
+  emailMessage: string;
+
+  private validationMessages = {
+    // key-value pair validation rule name
+    // the key name matches the key in error object collection so we can map this value to the key in error collection
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.'
+  };
 
   constructor(private fb: FormBuilder) { }
 
@@ -58,8 +70,26 @@ export class CustomerComponent implements OnInit {
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
       // set a default value of sendCatalog to true
-      sendCatalog: true
-    })
+      sendCatalog: true,
+      addressType: 'home',
+      street1: '',
+      street2: ''
+    });
+    
+    // call FormControl from the FormGroup
+    // track value changes in notification FormControl right after the component is initialised
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    // asynchronous - value passes in as the user types
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+      ).subscribe(
+      // each character will make a call this method
+      value => this.setMessage(emailControl)
+    );
   }
 
   populateTestData(): void {
@@ -74,6 +104,17 @@ export class CustomerComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  // AbstractControl is a value entered in an email input field (emailGroup.email)
+  setMessage(c: AbstractControl): void {
+    console.log(this.customerForm.get('emailGroup.email'));
+    this.emailMessage = '';
+    // check if the user modifies the field and entered input doesn't meet defined conditions
+    if((c.touched || c.dirty) && c.errors){
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
   }
 
   // this method is for a checkbox. 
